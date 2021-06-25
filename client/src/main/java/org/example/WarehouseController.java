1fa047135a8e8d6b691177055a4aec76291513fc
Roman Burakov <buracov.r@gmail.com>
2021-06-25 03:33:22 +0300

    add product gui

--- client/src/main/java/org/example/WarehouseController.java
+++ client/src/main/java/org/example/WarehouseController.java
@@ -1,13 +1,11 @@
 package org.example;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 import org.example.Entities.Category;
 import org.example.Entities.CategoryPane;
@@ -45,8 +43,8 @@ public class WarehouseController {
     private void addGroupButton() {
         System.out.println("Add group button!");
         ArrayList<ProductLabel> productLabels = new ArrayList<>();
-        Product p1 = new Product("PIVO", "pikvo", "ukraina", 10d, 1d, 2);
-        Product p2 = new Product("VODKA", "pikvo", "ukraina", 10d, 1d, 2);
+        Product p1 = new Product("PIVO", "pevko", "ukraina", 10d, 1d, 2);
+        Product p2 = new Product("VODKA", "vodochka", "ukraina", 10d, 1d, 2);
 
         productLabels.add(new ProductLabel(p1));
         productLabels.add(new ProductLabel(p2));
@@ -55,6 +53,19 @@ public class WarehouseController {
 
         CategoryPane pane = new CategoryPane(cat, productLabels);
         groupsUnitsVBox.getChildren().add(pane);
+
+
+        ArrayList<ProductLabel> productLabels2 = new ArrayList<>();
+        Product p12 = new Product("PIVO", "pevko", "ukraina", 10d, 1d, 2);
+        Product p22 = new Product("VODKA", "vodochka", "ukraina", 10d, 1d, 2);
+
+        productLabels2.add(new ProductLabel(p12));
+        productLabels2.add(new ProductLabel(p22));
+
+        Category cat2 = new Category("Buhlo2", "kak vodicka");
+
+        CategoryPane pane2 = new CategoryPane(cat2, productLabels);
+        groupsUnitsVBox.getChildren().add(pane2);
     }
 
     public static void removeCategoryPane(CategoryPane categoryPane) {
